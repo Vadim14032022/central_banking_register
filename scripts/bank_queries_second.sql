04aafa4efdf9af81294cae9c6b843c83948735dd
Vadim <semenov.vr@phystech.edu>
2024-03-25 23:52:12 +0300

    add inserts

--- scripts/bank_queries_second.sql
+++ scripts/bank_queries_second.sql
@@ -14,14 +14,14 @@ GROUP BY user_id
 HAVING COUNT(account_id) > 1
 ORDER BY COUNT(account_id) DESC;
 
--- 3) Запрос на добавление колонки с ранжированием 
+-- 3) Добавление колонки с ранжированием 
 --    по балансу счета внутри каждого типа счета,
 --    и с числом пользователей,
 --    для счетов с хотя бы двумя пользователями
 
 SELECT *,
        RANK() OVER(PARTITION BY acc_type_id ORDER BY cur_balance_amt DESC) AS balance_rank,
-       COUNT(*) OVER(PARTITION BY acc_type_id)
+       COUNT(*) OVER(PARTITION BY acc_type_id) AS num_users
 FROM (
     SELECT *,
        COUNT(*) OVER(PARTITION BY acc_type_id) as cnt
@@ -29,19 +29,19 @@ FROM (
 ) AS T 
 WHERE cnt >= 2;
 
--- 4) Запрос на добавление колонки с суммарным балансом 
+-- 4) Добавление колонки с суммарным балансом 
 --    для каждого типа счета, упорядоченного по убыванию суммарного баланса:
 SELECT acc_type_id,
        SUM(cur_balance_amt) OVER(PARTITION BY acc_type_id) AS total_balance
 FROM cbr.account
 ORDER BY total_balance DESC;
 
--- 5) Запрос на вывод самого высокого баланса счета для каждого типа счета:
+-- 5) Вывод самого высокого баланса счета для каждого типа счета:
 SELECT *,
        MAX(cur_balance_amt) OVER(PARTITION BY acc_type_id) AS max_balance
 FROM cbr.account;
 
--- 6) Запрос на вывод номера телефона клиента с самым длинным списком 
+-- 6) Вывод номера телефона клиента с самым большим числом 
 --    счетов, упорядоченным по убыванию количества счетов:
 SELECT phone_num, COUNT(cbr.registry.account_id) AS num_accounts,
        ROW_NUMBER() OVER(ORDER BY COUNT(cbr.registry.account_id) DESC) AS phone_rank
@@ -49,3 +49,4 @@ FROM cbr.registry
 JOIN cbr.user ON cbr.registry.user_id = cbr.user.user_id
 GROUP BY phone_num
 ORDER BY num_accounts DESC;
+
